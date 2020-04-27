650a5ef2ad4801948e2ebe68a9e791ba96ad1391
kaylawilson535 <42094645+kaylawilson535@users.noreply.github.com>
2020-04-27 16:36:45 -0600

    Painful Progress

--- wheres-the-tp/src/About.js
+++ wheres-the-tp/src/About.js
@@ -4,7 +4,16 @@ import './App.css';
 function About() {
   return (
     <div>
-        <h1>About Page</h1>
+        <h1>Who are we?</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a dictum sapien. Mauris metus eros, eleifend eget facilisis in, varius in erat. Ut at accumsan nisl. Phasellus efficitur ipsum at nunc condimentum feugiat. Donec blandit consequat sapien a euismod. Aenean at tincidunt sapien. Donec at enim ut mauris aliquet scelerisque. </p>
+            
+            <p>Vestibulum pharetra augue sit amet urna mattis tempor. Pellentesque efficitur dui eget laoreet luctus. Fusce id commodo purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam in rutrum nisl. Morbi et augue urna. Ut in arcu ex.</p>
+
+            <p>
+            Aliquam nec ultrices augue, eu elementum nibh. Maecenas vel ullamcorper tellus. Quisque volutpat pulvinar ligula eu dictum. Donec eget tellus nibh. Nunc accumsan mollis lacus, quis venenatis orci dignissim nec. Donec a facilisis ligula. Proin laoreet, ligula eget cursus bibendum, erat tellus ultrices velit, ac mattis risus dui quis mauris. Phasellus et blandit arcu. Aliquam mollis pulvinar mollis. Quisque facilisis nulla vel urna posuere mattis.
+            </p>
+
     </div>
   );
 }
--- wheres-the-tp/src/Merchants.js
+++ wheres-the-tp/src/Merchants.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
-function Merchants() {
-  return (
-    <div>
-        <h1>Merchants Page</h1>
-    </div>
-  );
-}
+export default function Merchants() {
 
-export default Merchants;
+    return(
+        <form>
+            <input type="text" placeholder="Merchant's ID" name="merchantId" />
+            <input type="text" placeholder="MerchantName" name="merchantName" />
+            <input type="text" placeholder="Merchant's Address" name="merchantAddress" />
+            <input type="text" placeholder="Yes/No" name="isStocked" />
+
+        </form>
+    )
+}
