4c067660ac30fc40b71b63a7129959842fac0280
kaylawilson535 <42094645+kaylawilson535@users.noreply.github.com>
2020-04-27 23:11:45 -0600

    boutta try something

--- wheres-the-tp/src/Merchants.js
+++ wheres-the-tp/src/Merchants.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import './App.css';
 import MerchantsForm from './MerchantsForm';
-import MerchantsTable from './MerchantsTable';
 
-function Merchants() {
-  return (
-    <div>
-        <MerchantsForm/>
-        <MerchantsTable/>
-    </div>
-  );
+
+class Merchants extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            merchants: [
+                {
+                    merchantID: 'TarJay231',
+                    merchantName: 'Target Store 231',
+                    merchantAddress: 'Targee Rd',
+                    isStocked: false
+                },
+                {
+                    merchantID: 'Alb23',
+                    merchantName: 'Albertsons Store 145',
+                    merchantAddress: 'Overland',
+                    isStocked: false
+                }
+            ]
+        };
+    }
+
+
+    parentFunction=(data_from_child)=>{
+        //console.log(data_from_child.merchantName.value);
+        //this.newMerchant.merchantID = data_from_child.merchantId.value;
+
+        this.setState({merchants:data_from_child});
+        //console.log(this.newMerchant.merchantId);
+    }
+   
+    render(){
+        return(
+            <div>
+                <MerchantsForm functionCallFromParent={this.parentFunction.bind(this)}/>
+                
+               
+            </div>
+        );      
+    }
+// <Todos merchants={this.state.merchants} />
+
 }
 
-export default Merchants;
+export default Merchants
