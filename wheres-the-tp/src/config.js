cf880b0eb8bbcd58cb1f202d6766b4fcdcfd9a84
Jonathan <jonathantipton@u.boisestate.edu>
2020-04-28 01:58:24 -0600

    FIRE! base server added to merchants.

---
+++ wheres-the-tp/src/config.js
@@ -0,0 +1,8 @@
+const config = {
+    apiKey: "AIzaSyAA77LQpxO-Xq2Z6K4FfE3wmxIWYUH_6Sk",
+    authDomain: "tp-tracker-ff15b.firebaseapp.com",
+    databaseURL: "https://tp-tracker-ff15b.firebaseio.com"
+  };
+  
+  export default config;
+  
